b48f0bcef36c4ae4cad3614efcc10268865ca71f
Warren <joshua.warren@yale.edu>
2021-05-15 13:15:34 -0400

    More spherical fix.

--- src/indicator_fun.cpp
+++ src/indicator_fun.cpp
@@ -15,9 +15,11 @@ arma::vec indicator_fun(int h_model,
 int n = distance_to_ps.size();
 arma::vec indicator_temp(n); indicator_temp.fill(0.00);
 double eta_full = 0.00;
+arma::vec eta_full_vec(n); eta_full_vec.fill(0.00);
 for(int j = 0; j < n; ++j){
    
    eta_full = eta(angle_key(j) - 1);
+   eta_full_vec(j) = eta_full;
    indicator_temp(j) = (distance_to_ps(j) <= lambda*exp(eta_full));
    if(distance_to_ps(j) == 0.00){
      indicator_temp(j) = 1.00;
@@ -44,8 +46,8 @@ if(h_model == 3){ //Gaussian
 
 if(h_model == 4){ //Spherical
   indicator = (1.00 +
-               -1.50*distance_to_ps/exp(eta_full) +
-               0.50*pow((distance_to_ps/exp(eta_full)), 3))%indicator_temp;
+               -1.50*distance_to_ps/exp(eta_full_vec) +
+               0.50*pow((distance_to_ps/exp(eta_full_vec)), 3))%indicator_temp;
   }
  
 return indicator;
